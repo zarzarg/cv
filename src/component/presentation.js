dfdac5428cca0b19e2ef0d8ee10a02fde520f62e
zarzarg <74294849+zarzarg@users.noreply.github.com>
2022-01-11 11:14:30 +0100

    Update presentation.js

--- src/component/presentation.js
+++ src/component/presentation.js
@@ -13,7 +13,7 @@ const Presentation = () => {
                 <span className="typing3">J'ai obtenu une licence en Biologie Environnement et Écologie en 2014</span>
                 <span className="typing4">Après de nombreuse années à travailler dans des emplois alimentaires, j'ai décidé de me reconvertir dans le Développement Web</span>
                 <span className="typing5">J'ai trouvé la voie professionnelle qui me passionne en obtenant un Bac+2 avec mention en 2021</span>
-                <span className="typing6">Je poursuivrai cette reconversion professionnelle de façon a m'épanouir </span>
+                <span className="typing6">Je poursuivrai cette reconversion professionnelle de façon à m'épanouir </span>
             </div>
         </>
     )
